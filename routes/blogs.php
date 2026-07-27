9585a35c3f9a2b4c601926b846a6a713fcc86962
Arsh <arsshsingh0@gmail.com>
2026-07-27 19:30:44 +0530

    added .php in blog url

--- routes/blogs.php
+++ routes/blogs.php
@@ -3,5 +3,5 @@ use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/education', [BlogController::class, 'blogs'])->name('blogs');
-Route::get('/blogs/full_blog', [BlogController::class, 'show'])
+Route::get('/blogs/full_blog.php', [BlogController::class, 'show'])
     ->name('blogs.show');
